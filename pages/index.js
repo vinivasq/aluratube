e5b395aaba6f743f83c3a968f7526af4369de238
vinivasq <vinivasq@hotmail.com>
2022-11-09 20:35:36 -0300

    Providers e dark mode

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import config from "../config.json";
 import styled from "styled-components";
-import { CSSReset } from "../src/components/CSSReset.js";
 import Menu from "../src/components/Menu/Menu";
 import { StyledTimeline } from "../src/components/Timeline";
 
@@ -10,7 +9,6 @@ function HomePage() {
   
   return (
     <>
-      <CSSReset />
       <div
         style={{
           display: "flex",
@@ -29,6 +27,8 @@ function HomePage() {
 export default HomePage;
 
 const StyledHeader = styled.div`
+  background-color: ${({ theme }) => theme.backgroundLevel1};
+
   img {
     width: 80px;
     height: 80px;
